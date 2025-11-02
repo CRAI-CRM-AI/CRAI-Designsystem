32af5e5991e7174c60bb0b563337182e45ae51a8
HenrikFalla <henrik.falla@gmail.com>
2025-11-02 19:13:58 +0100

    Updated namespace

--- packages/designsystem-core/stencil.config.ts
+++ packages/designsystem-core/stencil.config.ts
@@ -2,7 +2,7 @@ import { Config } from '@stencil/core';
 import { reactOutputTarget } from '@stencil/react-output-target';
 
 export const config: Config = {
-  namespace: 'designsystem-core',
+  namespace: 'crai-designsystem-core',
   globalStyle: 'src/lib/global.css',
   outputTargets: [
     reactOutputTarget({
