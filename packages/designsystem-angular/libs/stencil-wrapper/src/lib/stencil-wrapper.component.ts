bbd75bc23a457fdedeaaf7030aa916971c58b21d
HenrikFalla <henrik.falla@gmail.com>
2025-11-08 18:05:55 +0100

    Angular project

---
+++ packages/designsystem-angular/libs/stencil-wrapper/src/lib/stencil-wrapper.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-stencil-wrapper',
+  imports: [],
+  template: `
+    <p>
+      stencil-wrapper works!
+    </p>
+  `,
+  styles: ``
+})
+export class StencilWrapperComponent {
+
+}
