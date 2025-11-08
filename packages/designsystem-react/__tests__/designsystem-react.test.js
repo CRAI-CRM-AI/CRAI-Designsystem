f2dbb1d45e18ccca129102aa09c4d362c6b7c426
HenrikFalla <henrik.falla@gmail.com>
2025-11-08 18:06:09 +0100

    React project

---
+++ packages/designsystem-react/__tests__/designsystem-react.test.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const designsystemReact = require('..');
+const assert = require('assert').strict;
+
+assert.strictEqual(designsystemReact(), 'Hello from designsystemReact');
+console.info('designsystemReact tests passed');
---
+++ packages/designsystem-react/lib/designsystem-react.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = designsystemReact;
+
+function designsystemReact() {
+  return 'Hello from designsystemReact';
+}
